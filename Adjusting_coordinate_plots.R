b0d9ccbbac08144a947a4f1d35d2445980a7628d
Andrew Mercadante <andrewmercadante@ufl.edu>
2021-06-02 18:47:28 -0400

    Managed to merge dataframes and create new columns x_final and y_final that represent the true position of the plants within the fragments.

--- Adjusting_coordinate_plots.R
+++ Adjusting_coordinate_plots.R
@@ -25,6 +25,7 @@ grid_coords_simple <-
             column = unique(column))
 ha2 <- left_join(ha, grid_coords_simple, by = "ha_id_number")
 
+
 # sort to find 1-ha habitats
 ha_1frag <-ha2 %>% 
   filter(habitat == "1-ha")
@@ -33,54 +34,56 @@ unique(ha_1frag$ranch)  # "Dimona" "Esteio-Colosso" and "PortoAlegre"
 
 PortoAlegre_1ha <-ha_1frag %>%
   filter(ranch == "PortoAlegre") %>%
-  select(ranch,plot,habitat,ha_id_number,year,row,column)
+  select(ranch,plot,habitat,ha_id_number,year,ht,row,column)
 
-PortoAlegre_1ha_layout <-  # assuming bottom-left is origin
+PortoAlegre_1ha_layout <-  # assuming bottom-left is origin, and E1 is at (0,0)
   expand_grid(
-    tibble(row = LETTERS[1:5],
-           plot_x = 0:4), # swapped x and y here since row and column variables are swapped. 
+    tibble(row = LETTERS[5:1],
+           plot_y = seq(0,40,10)),  
     tibble(column = 1:10,
-           plot_y = 0:9)
+           plot_x = seq(0,90,10))
   ) %>%
   add_column(plot = "5753")
 
-left_join(PortoAlegre_1ha,PortoAlegre_1ha_layout)
+PortoAlegre_1ha_join <-left_join(PortoAlegre_1ha,PortoAlegre_1ha_layout)
 
 
 # Dimona layout from data-wrangling example 
 
 dimona_1ha_1 <- 
   ha2 %>%
-  filter(plot == first(plot)) %>% 
+  filter(plot == "2107") %>% 
   select(ranch, plot, habitat, ha_id_number, year, ht, row, column)
 
-dimona_1_layout <- # Assumes bottom-left origin
+dimona_1_layout <- # Assumes bottom-left origin and E1 is at (0,0)
   expand_grid(
     tibble(row = LETTERS[5:1], 
-           plot_x = 4:0), # swapped position of plot_x and plot_y to reflect data and diagrams
+           plot_y = seq(0,40,10)), 
     tibble(column = 1:10,
-           plot_y = 0:9)
+           plot_x = seq(0,90,10))
   ) %>%
   add_column(plot = "2107")
+
 dimona_1_layout
-left_join(dimona_1ha_1,dimona_1_layout)
+dimona_1ha_1_join <- left_join(dimona_1ha_1,dimona_1_layout)
 
 dimona_1ha_2 <- #dimona 2nd 1-ha plot 
   ha2 %>%
   filter(plot == "2108") %>%
   select(ranch, plot, habitat, ha_id_number, year, ht, row, column)
 
-dimona_2_layout <- # Assumes top-right origin
+dimona_2_layout <- # Assumes bottom-left origin with A10 at (0,0)
   expand_grid(
     tibble(row = LETTERS[1:5], 
-           plot_x = 0:4), # swapped position of plot_x and plot_y to reflect data and diagrams
-    tibble(column = 1:10,
-           plot_y = 0:9)
+           plot_y = seq(0,40,10)), 
+    tibble(column = 10:1,
+           plot_x = seq(0,90,10))
   ) %>%
   add_column(plot = "2108")
 dimona_2_layout
 
-left_join(dimona_2_layout,dimona)
+dimona_1ha_2_join <- left_join(dimona_1ha_2,dimona_2_layout)
+
 
 # Writing function to repeat filtering task ----------------------------------
 
@@ -100,13 +103,56 @@ ha_data_filter(ha2,"Esteio-Colosso","CF")
 Esteio_Colosso_1ha <- ha_data_filter(ha2,"Esteio-Colosso","1-ha")
 unique(Esteio_Colosso_1ha$plot)
 
-# assume bottom-right point of origin
+# assume bottom-right point of origin and A10 at (0,0)
 EST_COL_1ha_layout <-
   expand_grid(
     tibble(row = LETTERS[1:5], 
-           plot_x = 0:4), # swapped position of plot_x and plot_y to reflect data and diagrams
-    tibble(column = 1:10,
-           plot_y = 0:9)
+           plot_y =seq(0,40,10)), 
+    tibble(column = 10:1,
+           plot_x = seq(0,90,10))
   ) %>%
     add_column(plot="5751")
-left_join(Esteio_Colosso_1ha,EST_COL_1ha_layout)
+EST_COL_1ha_join<-left_join(Esteio_Colosso_1ha,EST_COL_1ha_layout)
+
+
+
+# test to figure out if id in excel sheet is the same as ha_id_number --
+
+ha2 %>%
+  filter(plot=="2107") %>%
+  filter(ha_id_number=="100") %>%
+  select(ha_id_number,row,column)
+
+ha2 %>%
+  filter(plot=="2107") %>%
+  filter(ha_id_number == "262") %>%
+  select(ha_id_number,row,column)
+
+# seems like the plantID doesn't correspond with the ha_id_number in dataframe ha2
+
+
+# test rbind() and gather all joins from 1-ha fragments into a single df 
+Joint_data <- rbind(dimona_1ha_1_join,dimona_1ha_2_join)
+Joint_data2 <- rbind(EST_COL_1ha_join,PortoAlegre_1ha_join)
+Joint_data_final <- rbind(Joint_data,Joint_data2) 
+
+ha3 <- left_join(ha2,Joint_data_final)
+?join
+
+# group coordinates in sub-plot by ha_id_number and summarize x y columns
+simple_coords <-coords %>%
+  group_by(ha_id_number) %>%
+  summarize(x,y) %>%
+  view()
+# join dataframe with coordinates based on row column combinations w/ subplot coords
+ha3 <-left_join(ha2,simple_coords)  
+
+# Join this dataframe with the row-column coords for all the 1-ha fragments. 
+ha4 <- left_join(ha3,Joint_data_final)
+
+one_ha_coord_final<-
+mutate(ha4,x_final = plot_x + x) %>%
+mutate(ha4,y_final = plot_y + y) %>%
+  view()
+
+
