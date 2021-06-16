81a971e54284dea08995d3149f72f3405e9d583f
Andrew Mercadante <andrewmercadante@ufl.edu>
2021-06-16 09:30:35 -0400

    Removed old method, added additional documentation for the functionality of loops and variables, and improved reproducibility using "package::function" notation.

--- Calc_Dist_to_Edge_20210607.R
+++ Calc_Dist_to_Edge_20210607.R
@@ -75,154 +75,86 @@ length(unique(EST_1_ha$ha_id_number)) ==
   length(unique(EST_simple$ha_id_number))
 
 
-# calculations of distance to edge  ---------------------------------------
-
-# Better description needed of what this needs.
-
-# list of equidistant points for Colosso, can define each edge orientation by what 
-# is below the curve, to the left, to the right, and above and below the curve.
-
-test_var <-seq(20,80,1)
-y <- seq(0,30,1)
-y2 <- sort(y,decreasing=TRUE)
-y2 <- y2[-1]
-y_final <- c(y,y2)
-
-y_final
-
-plot(test_var,y_final,lty=1,type="b")
-
-# Can describe the function, then use it to test against points
-#to then determine what edge they are closest to and what method to use to find linear distance
-
-# its an absolute value function of the form: y = -|x-50|+30
-
-abs_value_Colosso <- function(x_value) {
-  y = -abs(x_value-50)+30
-  return(y)
-}
-
-abs_value_Colosso(20)
-
-
-# test a conditional with this function
-for (i in 1:192) {
-if(abs_value_Colosso(EST_simple$x[i]) > EST_simple$y[i]) {
-  print("Yes")
-
-}
-  else {
-    print("no")
-  }
-}  
-warnings() 
-EST
-EST_x <- EST_simple$x
-EST_y <- EST_simple$y
-Edge1_Orientation <-NULL
-Edge2_Orientation <- NULL
-distance_to_nearest_edge <- NULL
-distance_to_next_nearest_edge <- NULL
-ha_id_number <- EST_x_over50$ha_id_number
-
-# split Colosso data into three cases based on domain values of x 
-EST_x_under50 <- EST_simple[EST_simple$x < 50,]
-EST_x_over50 <- EST_simple[EST_simple$x >50,]
-EST_x_at50 <- EST_simple[EST_simple$x == 50,] # 0 observations
-
-for(i in 1:72) {
-  if (EST_x_over50$y[i] > abs_value_Colosso(EST_x_over50$x[i])){
-    distance_to_nearest_edge[i] <- 101-EST_x_over50$x[i]
-    Edge1_Orientation[i] <- "West"
-  }
-  else if (EST_x_over50$y[i] < abs_value_Colosso(EST_x_over50$x[i])){
-    distance_to_nearest_edge[i] <- EST_x_over50$y[i] + 20
-    Edge1_Orientation[i] <- "North"
-    
-  }
-  else if (EST_x_over50$y[i] == abs_value_Colosso(EST_x_over50$x[i])){
-    distance_to_nearest_edge[i] <- EST_x_over50$y[i] + 20
-    Edge1_Orientation[i] <- "Equidistant; NW"
-  }
-}
-
-
 # ----------------------------- New Method, exhaustive --------------
 
 EST_simple
-distance_to_N_edge <- NULL
+distance_to_N_edge <- NULL # set up variables to store distances to all edges
 distance_to_S_edge <- NULL
 distance_to_E_edge <- NULL
 distance_to_W_edge <- NULL
 
-for (i in 1:192) {
-  distance_to_N_edge[i] <- EST_simple$y[i] + 20
+for (i in 1:length(EST_simple$x)) { # for loop that calculates linear distance to each edge
+  distance_to_N_edge[i] <- EST_simple$y[i] + 20 
   distance_to_E_edge[i] <- EST_simple$x[i] 
   distance_to_W_edge[i] <-100-EST_simple$x[i]
   distance_to_S_edge[i] <- EST_simple$y[i] + 50
 }
 
 
-ha_id_number <- EST_simple$ha_id_number
+ha_id_number <- EST_simple$ha_id_number # need to make this a variable so I can add it  
+                                        # into a new dataframe to allow for a join later. 
+
 distances <- data.frame(ha_id_number,distance_to_N_edge,distance_to_E_edge,
                         distance_to_W_edge,distance_to_S_edge)
+
 Colosso_1ha <- left_join(EST_simple,distances) # join dataframes
 
 #distances$dist_to_nearest_edge <- apply(distances,1,FUN=min) this is how to get first edge
 
 # How to get second closest edge?
-distance_to_nearest_edge <- NULL
+distance_to_nearest_edge <- NULL # set up variables to store the two lowest values from the 4 distances
 distance_to_next_nearest_edge <- NULL
 
-for(i in 1:192){
-distances$distance_to_nearest_edge[i] <-apply(distances[i,2:5],1,FUN=min)
+for(i in 1:length(EST_simple$x)){
+distances$distance_to_nearest_edge[i] <-apply(distances[i,2:5],1,FUN=min) # apply min() to 2nd-5th column of ith row
 distance.temp <- as.matrix(distances[i,2:5]) # convert to matrix so nth() can work
-distances$distance_to_next_nearest_edge[i] <- nth(distance.temp,2,descending=F)
+distances$distance_to_next_nearest_edge[i] <- Rfast::nth(distance.temp,2,descending=F) # run nth() to select 2nd value 
+                                                                                       # in a vector of increasing distances 
 }
-warnings()
-Colosso_1ha <- left_join(EST_simple,distances)
+
+Colosso_1ha <- left_join(EST_simple,distances) # join the previous Colosso data with newly aquired distances df
 
 # Porto Alegre 1-ha fragment -----------------------------------------------
 
 
-distance_to_N_edge <- NULL
+distance_to_N_edge <- NULL # setup distance variables
 distance_to_E_edge <- NULL
 distance_to_W_edge <- NULL
 distance_to_S_edge <- NULL
 
-for ( i in 1:210) {
+for ( i in 1:length(Porto_Alegre_simple$x)) { # for loop for calculating distance to edge
   distance_to_N_edge[i] <- 50-Porto_Alegre_simple$y[i]
   distance_to_E_edge[i] <- Porto_Alegre_simple$x[i]
   distance_to_W_edge[i] <- 100- Porto_Alegre_simple$x[i]
   distance_to_S_edge[i] <- Porto_Alegre_simple$y[i] + 50
 }
 ha_id_number <- Porto_Alegre_simple$ha_id_number
-distances_Alegre <- data.frame(ha_id_number,distance_to_N_edge,distance_to_E_edge,
+distances_Alegre <- data.frame(ha_id_number,distance_to_N_edge,distance_to_E_edge, # form df of distances w/ ha_id_number
                                           distance_to_W_edge,distance_to_S_edge)
 
-distance_to_nearest_edge <- NULL
+distance_to_nearest_edge <- NULL # initialize variables to store two lowest distance values
 distance_to_next_nearest_edge <- NULL
 
-for(i in 1:210){
-  distances_Alegre$distance_to_nearest_edge[i] <-apply(distances_Alegre[i,2:5],1,FUN=min)
-  distance.temp <- as.matrix(distances_Alegre[i,2:5])
-  distances_Alegre$distance_to_next_nearest_edge[i] <- nth(distance.temp,2,descending=F)
+for(i in 1:length(Porto_Alegre_simple$x)){
+  distances_Alegre$distance_to_nearest_edge[i] <-apply(distances_Alegre[i,2:5],1,FUN=min) # apply min() to 2nd-5th columns of ith row
+  distance.temp <- as.matrix(distances_Alegre[i,2:5]) # store as matrix to allow nth() to work
+  distances_Alegre$distance_to_next_nearest_edge[i] <- Rfast::nth(distance.temp,2,descending=F)
 }
 
-Porto_Alegre_1ha_join <- left_join(Porto_Alegre_simple,distances_Alegre)
+Porto_Alegre_1ha_join <- left_join(Porto_Alegre_simple,distances_Alegre) # join two dfs
 
 
 # Dimona 1-ha fragment 2107 ------------------------------------------
 
-distance_to_N_edge <- NULL
+distance_to_N_edge <- NULL # same as above
 distance_to_E_edge <- NULL
 distance_to_W_edge <- NULL
 distance_to_S_edge <- NULL
 
-distance_to_nearest_edge <- NULL
+distance_to_nearest_edge <- NULL # same as above
 distance_to_next_nearest_edge <- NULL
 
-for (i in 1:177) {
+for (i in 1:length(Dimona_2107_1ha_simple$x)) { # for loop for calculating linear distance to edge
   distance_to_N_edge[i] <- Dimona_2107_1ha_simple$y[i] + 50
   distance_to_E_edge[i] <- Dimona_2107_1ha_simple$x[i]
   distance_to_W_edge[i] <- 100-Dimona_2107_1ha_simple$x[i]
@@ -230,14 +162,14 @@ for (i in 1:177) {
 }
 
 ha_id_number <- Dimona_2107_1ha_simple$ha_id_number
-distances_Dimona_2107 <- data.frame(ha_id_number,distance_to_N_edge,distance_to_E_edge,
+distances_Dimona_2107 <- data.frame(ha_id_number,distance_to_N_edge,distance_to_E_edge, # form df containing distances and ha_id_number
                                     distance_to_W_edge,distance_to_S_edge)
 
-for (i in 1:177) {
+for (i in 1:length(Dimona_2107_1ha_simple$x)) {
   distances_Dimona_2107$distance_to_nearest_edge[i] <- apply(distances_Dimona_2107
                                                              [i,2:5],1,FUN=min)
   distance.temp <- as.matrix(distances_Dimona_2107[i,2:5])
-  distances_Dimona_2107$distance_to_next_nearest_edge[i] <-nth(distance.temp,2,descending=F)
+  distances_Dimona_2107$distance_to_next_nearest_edge[i] <-Rfast::nth(distance.temp,2,descending=F)
 }
 
-Dimona_2107_1ha_join <- left_join(Dimona_2107_1ha_simple,distances_Dimona_2107)
+Dimona_2107_1ha_join <- left_join(Dimona_2107_1ha_simple,distances_Dimona_2107) # join dfs 
