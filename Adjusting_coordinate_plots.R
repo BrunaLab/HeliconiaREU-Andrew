3ced682ca4ac88b7d3c4ae21f8d8bbfefe963f93
Aariq <scottericr@gmail.com>
2021-07-14 09:24:40 -0400

    switch to newest data, already has row and column in it.

--- Adjusting_coordinate_plots.R
+++ Adjusting_coordinate_plots.R
@@ -11,19 +11,15 @@ coords
 grid_coords <-read_csv(here("data","grid_coords.csv"))
 
 ha <- 
-  read_csv(here("data", "cleaned_ha_data.csv"), col_types = cols(
+  read_csv(here("data", "ha_clean.csv"), col_types = cols(
     #make sure these columns don't get mistaken for numeric
     plot = col_character(), 
     bdffp_reserve_no = col_character()
   ))
 
 # Join grid coord data ----------------------------------------------------
-grid_coords_simple <- 
-  grid_coords %>% 
-  group_by(ha_id_number) %>% #only need a single row for each plant
-  summarize(row = unique(row),
-            column = unique(column))
-ha2 <- left_join(ha, grid_coords_simple, by = "ha_id_number")
+
+ha2 <- ha
 
 
 # sort to find 1-ha habitats
@@ -34,7 +30,7 @@ unique(ha_1frag$ranch)  # "Dimona" "Esteio-Colosso" and "PortoAlegre"
 
 PortoAlegre_1ha <-ha_1frag %>%
   filter(ranch == "PortoAlegre") %>%
-  select(ranch,plot,habitat,ha_id_number,year,ht,row,column)
+  dplyr::select(ranch,plot,habitat,ha_id_number,year,ht,row,column)
 
 PortoAlegre_1ha_layout <-  # assuming bottom-left is origin, and E1 is at (0,0)
   expand_grid(
@@ -53,7 +49,7 @@ PortoAlegre_1ha_join <-left_join(PortoAlegre_1ha,PortoAlegre_1ha_layout)
 dimona_1ha_1 <- 
   ha2 %>%
   filter(plot == "2107") %>% 
-  select(ranch, plot, habitat, ha_id_number, year, ht, row, column)
+  dplyr::select(ranch, plot, habitat, ha_id_number, year, ht, row, column)
 
 dimona_1_layout <- # Assumes bottom-left origin and E1 is at (0,0)
   expand_grid(
@@ -70,7 +66,7 @@ dimona_1ha_1_join <- left_join(dimona_1ha_1,dimona_1_layout)
 dimona_1ha_2 <- #dimona 2nd 1-ha plot 
   ha2 %>%
   filter(plot == "2108") %>%
-  select(ranch, plot, habitat, ha_id_number, year, ht, row, column)
+  dplyr::select(ranch, plot, habitat, ha_id_number, year, ht, row, column)
 
 dimona_2_layout <- # Assumes bottom-left origin with A10 at (0,0)
   expand_grid(
@@ -89,8 +85,8 @@ dimona_1ha_2_join <- left_join(dimona_1ha_2,dimona_2_layout)
 
 ha_data_filter <- function(data,ranch_name,habitat_type) {
   data %>%
-    filter(ranch == ranch_name & habitat == habitat_type) %>%
-    select(ranch, plot, habitat, ha_id_number, year, ht, row, column)
+    dplyr::filter(ranch == ranch_name & habitat == habitat_type) %>%
+    dplyr::select(ranch, plot, habitat, ha_id_number, year, ht, row, column)
 }
 
 # test function -----------------------------------------------------------
@@ -119,14 +115,14 @@ EST_COL_1ha_join<-left_join(Esteio_Colosso_1ha,EST_COL_1ha_layout)
 # test to figure out if id in excel sheet is the same as ha_id_number -------
 
 ha2 %>%
-  filter(plot=="2107") %>%
-  filter(ha_id_number=="100") %>%
-  select(ha_id_number,row,column)
+  dplyr::filter(plot=="2107") %>%
+  dplyr::filter(ha_id_number=="100") %>%
+  dplyr::select(ha_id_number,row,column)
 
 ha2 %>%
-  filter(plot=="2107") %>%
-  filter(ha_id_number == "262") %>%
-  select(ha_id_number,row,column)
+  dplyr::filter(plot=="2107") %>%
+  dplyr::filter(ha_id_number == "262") %>%
+  dplyr::select(ha_id_number,row,column)
 
 # seems like the plantID doesn't correspond with the ha_id_number in dataframe ha2
 
@@ -137,13 +133,13 @@ Joint_data2 <- rbind(EST_COL_1ha_join,PortoAlegre_1ha_join)
 Joint_data_final <- rbind(Joint_data,Joint_data2) 
 
 ha3 <- left_join(ha2,Joint_data_final)
-?join
+# ?join
 
 # group coordinates in sub-plot by ha_id_number and summarize x y columns
-simple_coords <-coords %>%
+simple_coords <- coords %>%
   group_by(ha_id_number) %>%
-  summarize(x,y) %>%
-  view()
+  summarize(x,y)
+# view(simple_coords)
 # join dataframe with coordinates based on row column combinations w/ subplot coords
 ha3 <-left_join(ha2,simple_coords)  
 
@@ -151,11 +147,12 @@ ha3 <-left_join(ha2,simple_coords)
 ha4 <- left_join(ha3,Joint_data_final)
 
 one_ha_coord_final<-
-mutate(ha4,x_final = plot_x + x) %>%
-mutate(ha4,y_final = plot_y + y) %>%
-  view()
+  ha4 %>% 
+  mutate(x_final = plot_x + x) %>%
+  mutate(y_final = plot_y + y)
+# view(one_ha_coord_final)
 
-?write_rds
+# ?write_rds
 write_rds(one_ha_coord_final,here("data","one_ha_coords_updated.rds"))
 
 
