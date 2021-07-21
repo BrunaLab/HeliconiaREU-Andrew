aafc9515f63a93566086dac45306642b4ea0a791
Andrew Mercadante <andrewmercadante@ufl.edu>
2021-07-21 13:24:54 -0400

    Made sure that models were consistent in using log_size_prev as independent variable

--- analysis/Finding_Edge_Effects_20210716.R
+++ analysis/Finding_Edge_Effects_20210716.R
@@ -20,12 +20,15 @@ dist_10m_surv <- dist_10m %>%
 str(dist_10m)
 dist_10m$year <- as.factor(dist_10m$year)
 dist_10m$bdffp_reserve_no <- as.factor(dist_10m$bdffp_reserve_no)
+dist_10m$surv <- as.factor(dist_10m$surv)
+dist_10m$flwr <- as.factor(dist_10m$flwr)
+dist_10m$flwr_prev <- as.factor(dist_10m$flwr_prev)
 # perform logistic regression on survival
 
-surv.mod <- glmer(surv~size_prev+dist_near+bdffp_reserve_no+(1|year),
+surv.mod <- glmer(surv~log_size_prev+dist_near+bdffp_reserve_no+(1|year),
                   data=dist_10m,family=binomial)
 
-surv.mod2 <- glm(surv~size_prev+dist_near+bdffp_reserve_no,data=dist_10m,family=binomial)
+surv.mod2 <- glm(surv~log_size_prev+dist_near+bdffp_reserve_no,data=dist_10m,family=binomial)
 
 z <- resid(surv.mod2,type="working")+surv.mod2$linear.predictors
 
@@ -42,7 +45,7 @@ piecewiseSEM::rsquared(surv.mod)
 
 # Flowering probability using 10m distances --------------------------
 
-flwr.mod <- glmer(flwr~size_prev+flwr_prev+dist_near+bdffp_reserve_no+(1|year),
+flwr.mod <- glmer(flwr~log_size_prev+flwr_prev+dist_near+bdffp_reserve_no+(1|year),
                   data=dist_10m,family=binomial)
 summary(flwr.mod)
 car::Anova(flwr.mod) # dist_near is not a significant predictor
@@ -61,29 +64,27 @@ check_model(size.mod) # log-transform fixed heteroscedasity, still some question
 
 car::Anova(size.mod) # dist_near significant predictor of log_transformed size. 
 
-
-size.mod2 <- lm(log_size~log_size_prev+dist_near+bdffp_reserve_no+year,data=dist_10m)
-summary(size.mod2) 
-
-
 # Now preform the same set of analysis using finer scale distances -----------
 
 dist_fine <- read_rds(here("data","full_1ha_location_data.rds"))
 
 dist_fine$year <- as.factor(dist_fine$year)
 dist_fine$bdffp_reserve_no <- as.factor(dist_fine$bdffp_reserve_no)
+dist_fine$flwr <- as.factor(dist_fine$flwr)
+dist_fine$flwr_prev <- as.factor(dist_fine$flwr_prev)
+dist_fine$surv <- as.factor(dist_fine$surv)
 
 
 # Logistic regression of survival using fine distance predictor --------------
 
-surv.fine <- glmer(surv~size_prev+distance_to_nearest_edge+bdffp_reserve_no+(1|year),
+surv.fine <- glmer(surv~log_size_prev+distance_to_nearest_edge+bdffp_reserve_no+(1|year),
                    data=dist_fine,family=binomial)
 summary(surv.fine)
 car::Anova(surv.fine) # distance_to_nearest_edge is a significant predictor
 check_model(surv.fine)
 piecewiseSEM::rsquared(surv.fine)
 
-surv.fine2 <- glm(surv~size_prev+distance_to_nearest_edge+bdffp_reserve_no,
+surv.fine2 <- glm(surv~log_size_prev+distance_to_nearest_edge+bdffp_reserve_no,
                   data=dist_fine,family=binomial)
 
 summary(surv.fine2)
@@ -91,10 +92,12 @@ car::Anova(surv.fine2) # dist_near still significant
 
 # Logistic regression of flowering using fine distance predictor---------------
 
-flwr.fine <- glmer(flwr~size_prev+flwr_prev+distance_to_nearest_edge+bdffp_reserve_no+
+flwr.fine <- glmer(flwr~log_size_prev+flwr_prev+distance_to_nearest_edge+bdffp_reserve_no+
                      (1|year),data=dist_fine,family=binomial)
 summary(flwr.fine)
-car::Anova(flwr.fine) # distance_to_nearest_edge not significant predictor
+car::Anova(flwr.fine) # distance_to_nearest_edge signficant predictor when log_size_prev used,
+                      # but not signficant when size_prev used. Got convergence warnings when size_prev
+                      # was used, maybe scaled variable through log-transform makes REML easier.
 check_model(flwr.fine)
 piecewiseSEM::rsquared(flwr.fine)
 # Linear relationship between log_size and distance using fine distance predictor ---
@@ -118,7 +121,7 @@ car::Anova(size.simple)
 model.diag.size <- broom::augment(size.fine)
 
 ggplot(data=model.diag.size,aes(x=distance_to_nearest_edge,y=log_size))+
-  geom_point()+
+  geom_point(aes(color=bdffp_reserve_no))+
   stat_smooth(method=lm,se=FALSE)
 
 
@@ -151,8 +154,8 @@ surv.fine2.df <- broom::augment(surv.fine2,
                                 type.predict="response")
 
 ## Eric's notes ##
-newdata = data.frame(size_prev = mean(dist_fine$size_prev, na.rm = TRUE),
-                     distance_to_nearest_edge = seq(0, 50, by = 1),
+newdata = data.frame(log_size_prev = mean(dist_fine$log_size_prev, na.rm = TRUE),
+                     distance_to_nearest_edge = seq(0, 50, by = 0.1),
                      bdffp_reserve_no = unique(dist_fine$bdffp_reserve_no))
 
 broom::augment(surv.fine2, type.predict = "response", newdata = newdata) %>%
@@ -170,7 +173,7 @@ ggplot(data=surv.fine2.df,aes(x=distance_to_nearest_edge))+
 
 # Results ---------------------------------------------------------------------
 
-# Distance to nearest edge was a significant predictor of survival using finer distances
+# Distance to nearest edge was a significant predictor of survival and flowering using finer distances
 
 # DOI hard to interpret, have overlap almost instantly between base survival from CF and
 # values for FF. Looks like there is more variance in vital rate of survival for plants
